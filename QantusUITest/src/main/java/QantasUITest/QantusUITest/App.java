2cc15639ab4bfb013f270183ef341a4668d85978
seema gowda <seemag05@gmail.com>
2019-07-08 17:44:26 +1000

    Project Setup

---
+++ QantusUITest/src/main/java/QantasUITest/QantusUITest/App.java
@@ -0,0 +1,13 @@
+package QantasUITest.QantusUITest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
